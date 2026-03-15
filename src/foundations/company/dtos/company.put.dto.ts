79731006d79119d6e881f4db3c1ac7c21dbd96d1
Carlo Nicora <carlo.nicora@gmail.com>
2026-03-15 15:35:57 +0100

    feat(company): add address and fiscal_data fields to PUT DTO

--- src/foundations/company/dtos/company.put.dto.ts
+++ src/foundations/company/dtos/company.put.dto.ts
@@ -28,6 +28,46 @@ export class CompanyPutAttributesDTO {
   @IsOptional()
   @IsString()
   configurations?: string;
+
+  @IsOptional()
+  @IsString()
+  legal_address?: string;
+
+  @IsOptional()
+  @IsString()
+  street_number?: string;
+
+  @IsOptional()
+  @IsString()
+  street?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  province?: string;
+
+  @IsOptional()
+  @IsString()
+  region?: string;
+
+  @IsOptional()
+  @IsString()
+  postcode?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  country_code?: string;
+
+  @IsOptional()
+  @IsString()
+  fiscal_data?: string;
 }
 
 export class CompanyPutRelationshipsDTO {
