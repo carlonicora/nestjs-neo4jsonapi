78636b9f2edba59f1e31084218d598af9aad6e89
Carlo Nicora <carlo.nicora@gmail.com>
2026-04-19 23:56:47 +0200

    feat(assistant-message): add DataMeta

---
+++ src/foundations/assistant-message/entities/assistant-message.meta.ts
@@ -0,0 +1,8 @@
+import { DataMeta } from "../../../common/interfaces/datamodel.interface";
+
+export const assistantMessageMeta: DataMeta = {
+  type: "assistant-messages",
+  endpoint: "assistant-messages",
+  nodeName: "assistantMessage",
+  labelName: "AssistantMessage",
+};
